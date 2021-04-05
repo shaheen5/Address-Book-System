c05cff09173c39cd91b9f8654e7534863154fe03
shaheen5 <miyashaheen1995@gmail.com>
2021-04-05 17:27:48 +0530

    Added : method to edit person details by firstname

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -30,6 +30,39 @@ class Person{
 				+ ", State=" + state + ", ZipCode=" + zipCode + ", PhoneNumber=" + phoneNumber + ", Email=" + email
 				+ "]";
 	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 }
 
 public class AddressBookMain {
@@ -37,11 +70,23 @@ public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Program !!!");
 		Scanner sc=new Scanner(System.in);
-		Person person=addPerson(sc);
-		System.out.println(person);
+		Person person = null;
+		int choice;
+		do {
+			System.out.println("\n\n********OPTIONS*********\n1]Add Contact\n2]Edit Contact\n3]Exit");
+			choice=sc.nextInt();
+			switch(choice) {
+				case 1:person=addPerson(sc);
+						break;
+				case 2:editPerson(person,sc);
+						break;
+				case 3:System.exit(0);
+			}
+		}while(choice!=3);
 	}
 	public static Person addPerson(Scanner sc) {
 		System.out.println("\nEnter First name :- ");
+		sc.nextLine();
 		String firstName=sc.nextLine();
 		while(!firstName.matches("[A-Za-z]{3}[A-Za-z]*")) {
 			System.out.println("Minimum 3 characters required !\nEnter First name :- ");
@@ -79,7 +124,7 @@ public class AddressBookMain {
 		}
 		System.out.println("Enter Contact number :- ");
 		String phoneNumber=sc.nextLine();
-		while(!phoneNumber.matches("\\d{10}")) {
+		while(!phoneNumber.matches("^[8-9]{1}\\d{9}")) {
 			System.out.println("INVALID !\nEnter Contact number :- ");
 			phoneNumber=sc.nextLine();
 		}
@@ -91,6 +136,62 @@ public class AddressBookMain {
 		    email=sc.nextLine();
 		}
 		Person person=new Person(firstName,lastName,address,city,state,zipCode,phoneNumber,email);
+		System.out.println(person);
 		return person;
+	}
+	public static void editPerson(Person person,Scanner sc) {
+		System.out.println("Enter name of the person to edit :");
+		sc.nextLine();
+		String name=sc.nextLine();
+		int flag=0;
+		if(name.equals(person.getFirstName())) {
+			System.out.println("Enter address :- ");
+	     	String address=sc.nextLine();
+	     	while(!address.matches("^[a-zA-Z\\d\\s\\-\\_]*$")) {
+				System.out.println("INVALID! \nEnter Address :- ");
+				address=sc.nextLine();
+			}
+			person.setAddress(address);
+			System.out.println("Enter city :- ");
+			String city=sc.nextLine();
+			while(!city.matches("^[a-zA-Z]*$")) {
+				System.out.println("INVALID!\nEnter City :- ");
+				city=sc.nextLine();
+			}
+			person.setCity(city);
+			System.out.println("Enter state :- ");
+			String state=sc.nextLine();
+			while(!state.matches("^[a-zA-Z\\s]*$")) {
+				System.out.println("INVALID!\nEnter State :- ");
+				state=sc.nextLine();
+			}
+			person.setState(state);
+			System.out.println("Enter zip code :- ");
+			String zipCode=sc.nextLine();
+			while(!zipCode.matches("\\d{6}")) {
+				System.out.println("INVALID!!\nEnter Zip code :- ");
+				zipCode=sc.nextLine();
+			}
+			person.setZipCode(zipCode);
+			System.out.println("Enter contact number :- ");
+			String phoneNumber=sc.nextLine();
+			while(!phoneNumber.matches("^[8-9]{1}\\d{9}")) {
+				System.out.println("INVALID !\nEnter Contact number :- ");
+				phoneNumber=sc.nextLine();
+			}
+			person.setPhoneNumber(phoneNumber);
+			System.out.println("Enter Email Id:-");
+			String email=sc.nextLine();
+			while(!email.matches("^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$")) {
+				System.out.println("INVALID EMAIL ID !\n Enter Valid Email Id:-");
+			    email=sc.nextLine();
+			}
+			person.setEmail(email);
+			flag=1;
+			System.out.println(person);
+		}
+		if(flag==0) {
+			System.out.println("This name does not exists!");
+		}
 	}
 }

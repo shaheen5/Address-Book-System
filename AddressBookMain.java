38a3f5447d199a9f43a3055ac86e3cceef5af6d6
unknown <miyashaheen1995@gmail.com>
2021-04-05 12:38:17 +0530

    Added :Used console to add & validate person details in addressbook

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,5 +1,7 @@
 package Address_Book_Problem;
 
+import java.util.Scanner;
+
 class Person{
 	String firstName;
 	String lastName;
@@ -34,7 +36,61 @@ public class AddressBookMain {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Program !!!");
-		Person person=new Person("Shaheen","Miya","Bhairav Nagar","Pune","Maharashtra","411015","9912345675","abc@gmail.com");
+		Scanner sc=new Scanner(System.in);
+		Person person=addPerson(sc);
 		System.out.println(person);
+	}
+	public static Person addPerson(Scanner sc) {
+		System.out.println("\nEnter First name :- ");
+		String firstName=sc.nextLine();
+		while(!firstName.matches("[A-Za-z]{3}[A-Za-z]*")) {
+			System.out.println("Minimum 3 characters required !\nEnter First name :- ");
+		    firstName=sc.nextLine();
+		}
+		System.out.println("Enter Last name :- ");
+		String lastName=sc.nextLine();
+		while(!lastName.matches("[A-Za-z]{3}[A-Za-z]*")) {
+			System.out.println("Minimum 3 characters required !\nEnter Last name :- ");
+		    lastName=sc.nextLine();
+		}
+		System.out.println("Enter Address :- ");
+		String address=sc.nextLine();
+		while(!address.matches("^[a-zA-Z\\d\\s\\-\\_]*$")) {
+			System.out.println("INVALID! \nEnter Address :- ");
+			address=sc.nextLine();
+		}
+		System.out.println("Enter City :- ");
+		String city=sc.nextLine();
+		while(!city.matches("^[a-zA-Z]*$")) {
+			System.out.println("INVALID!\nEnter City :- ");
+			city=sc.nextLine();
+		}
+		System.out.println("Enter State :- ");
+		String state=sc.nextLine();
+		while(!state.matches("^[a-zA-Z\\s]*$")) {
+			System.out.println("INVALID!\nEnter State :- ");
+			state=sc.nextLine();
+		}
+		System.out.println("Enter Zip code :- ");
+		String zipCode=sc.nextLine();
+		while(!zipCode.matches("\\d{6}")) {
+			System.out.println("INVALID!!\nEnter Zip code :- ");
+			zipCode=sc.nextLine();
+		}
+		System.out.println("Enter Contact number :- ");
+		String phoneNumber=sc.nextLine();
+		while(!phoneNumber.matches("\\d{10}")) {
+			System.out.println("INVALID !\nEnter Contact number :- ");
+			phoneNumber=sc.nextLine();
+		}
+		System.out.println("Enter Email Id:-");
+		String email=sc.nextLine();
+		String EMAIL_REGEX = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+		while(!email.matches(EMAIL_REGEX)) {
+			System.out.println("INVALID EMAIL ID !\n Enter Valid Email Id:-");
+		    email=sc.nextLine();
+		}
+		Person person=new Person(firstName,lastName,address,city,state,zipCode,phoneNumber,email);
+		return person;
 	}
 }
